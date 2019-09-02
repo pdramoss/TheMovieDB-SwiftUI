43eabee15a6fea2c1df30d9175932f4339330121
Pedro Ramos <pdramoss@gmail.com>
2019-09-01 23:27:01 -0500

    Initial Commit

---
+++ TheMovieDB-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheMovieDB-SwiftUI
+//
+//  Created by Pedro Ramos on 1/09/19.
+//  Copyright © 2019 Pedro Ramos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheMovieDB-SwiftUITests/TheMovieDB_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  TheMovieDB_SwiftUITests.swift
+//  TheMovieDB-SwiftUITests
+//
+//  Created by Pedro Ramos on 1/09/19.
+//  Copyright © 2019 Pedro Ramos. All rights reserved.
+//
+
+import XCTest
+@testable import TheMovieDB_SwiftUI
+
+class TheMovieDB_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
